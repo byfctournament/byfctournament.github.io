071e4b1d73d29dca853f729b3ea76d49ec0ca859
webmaster on Asus <webmaster@barmingyouth.co.uk>
2019-06-07 16:33:11 +0100

    Added github remote to update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 
 git add *
 git commit -m"Added updated results"
-git push 
+git push
+
+## Do this to update the github version. Useful if DNS setings 
+## have not updated for all providers (Virgin seems to be slow)
+## git remote add github git@github.com:byfctournament/byfctournament.github.io.git
+git push github master
